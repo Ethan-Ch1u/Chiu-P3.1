8813c74bea0e72c0a65407ff4ddedd3baded4ef1
Ethan-Ch1u <92735665+Ethan-Ch1u@users.noreply.github.com>
2023-03-05 21:15:30 -0500

    Milestone 7

--- site.js
+++ site.js
@@ -45,6 +45,48 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dataArray){
+                  let month = '';
+                  switch(dataArray[1]){
+                        case 1:
+                              month = 'January';
+                              break;
+                         case 2:
+                              month = 'Febusry';
+                              break;
+                         case 3:
+                              month = 'March';
+                              break;
+                         case 4:
+                              month = 'April';
+                              break;
+                         case 5:
+                              month = 'May';
+                              break;
+                         case 6:
+                              month = 'June';
+                              break;
+                         case 7:
+                              month = 'July';
+                              break;
+                         case 8:
+                              month = 'August';
+                              break;
+                         case 9:
+                              month = 'September';
+                              break;
+                         case 10:
+                              month = 'October';
+                              break;
+                         case 11:
+                              month = 'November';
+                              break;
+                         case 12:
+                              month = 'December';
+                              break; 
+                  }
+                  return month + ", " + dataArray[2] + ', ' + dataArray[1]
+            }
       }
 })
 
